6444cc279961899b964860e6587cdea04e55dacd
turzo1061 <turzo15-5785@diu.edu.bd>
2025-03-25 13:55:34 +0600

    problem10

--- tracker_sheet_problems-ec3a87df2a65eda53dead0899a1bc8a41b5bae18/lib/main.dart
+++ tracker_sheet_problems-ec3a87df2a65eda53dead0899a1bc8a41b5bae18/lib/main.dart
@@ -10,154 +10,159 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Custom Card App',
+      title: 'Bottom Navigation Bar App',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
 
-  final List<Map<String, String>> items = [
-    {
-      'image':
-          'https://images.unsplash.com/photo-1579952363873-27f3bade9f55?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3BvcnRzfGVufDB8fDB8fHww',
-      'title': 'Football',
-      'subtitle': 'Footaball is best',
-      'description': 'Football Seasson is Coming',
-    },
-    {
-      'image':
-          'https://plus.unsplash.com/premium_photo-1684820878202-52781d8e0ea9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c3BvcnRzfGVufDB8fDB8fHww',
-      'title': 'Cycle Race',
-      'subtitle': 'Speed will blow your mind',
-      'description': 'It is hard.',
-    },
-    {
-      'image':
-          'https://plus.unsplash.com/premium_photo-1685366454581-796f5fc832c6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHNwb3J0c3xlbnwwfHwwfHx8MA%3D%3D',
-      'title': 'Basketball',
-      'subtitle': 'Perfect for sunny weather',
-      'description': 'This is the Basketball',
-    },
-    {
-      'image':
-          'https://images.unsplash.com/photo-1530549387789-4c1017266635?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHNwb3J0c3xlbnwwfHwwfHx8MA%3D%3D',
-      'title': 'Swimming',
-      'subtitle': 'Good for  Health',
-      'description': 'This is the description for Swimming',
-    },
-  ];
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _tabs = [HomeTab(), SearchTab(), ProfileTab()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sports List'),
+        title: Text('Bottom Navigation Bar'),
         centerTitle: true,
-        backgroundColor: Colors.blue[300],
+        backgroundColor: Colors.blue[400],
       ),
-      body: ListView.builder(
-        padding: EdgeInsets.all(16),
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          return CustomCard(
-            image: items[index]['image']!,
-            title: items[index]['title']!,
-            subtitle: items[index]['subtitle']!,
-            description: items[index]['description']!,
-          );
+      body: _tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
         },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
       ),
     );
   }
 }
 
-class CustomCard extends StatefulWidget {
-  final String image;
-  final String title;
-  final String subtitle;
-  final String description;
-
-  CustomCard({
-    required this.image,
-    required this.title,
-    required this.subtitle,
-    required this.description,
-  });
-
-  @override
-  _CustomCardState createState() => _CustomCardState();
-}
-
-class _CustomCardState extends State<CustomCard> {
-  bool _isHovered = false;
-
+class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      onEnter: (_) => setState(() => _isHovered = true),
-      onExit: (_) => setState(() => _isHovered = false),
-      child: GestureDetector(
-        onTap: () {
-          // Add action on tap
-        },
-        child: AnimatedContainer(
-          duration: Duration(milliseconds: 200),
-          margin: EdgeInsets.only(bottom: 16),
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            color: Colors.grey[200],
-            borderRadius: BorderRadius.circular(8),
-            boxShadow:
-                _isHovered
-                    ? [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 10,
-                        offset: Offset(0, 4),
-                      ),
-                    ]
-                    : [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 4,
-                        offset: Offset(0, 2),
-                      ),
-                    ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(
+            'https://plus.unsplash.com/premium_photo-1689609950112-d66095626efb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG9tZXxlbnwwfHwwfHx8MA%3D%3D',
+            width: 150,
+            height: 150,
+            fit: BoxFit.cover,
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.network(
-                  widget.image,
-                  height: 150,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              SizedBox(height: 16),
-              Text(
-                widget.title,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 8),
-              Text(
-                widget.subtitle,
-                style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-              ),
-              SizedBox(height: 8),
-              Text(
-                widget.description,
-                style: TextStyle(fontSize: 14, color: Colors.grey[800]),
-              ),
-            ],
+          SizedBox(height: 16),
+          Text(
+            'Home Tab',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue[800],
+            ),
           ),
-        ),
+          SizedBox(height: 8),
+          Text(
+            'Welcome to the Home tab. Explore our content.',
+            style: TextStyle(
+              fontSize: 16,
+              fontStyle: FontStyle.italic,
+              color: Colors.blue[600],
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(
+            'https://plus.unsplash.com/premium_photo-1681487857509-abb58345e594?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c2VhcmNofGVufDB8fDB8fHww',
+            width: 150,
+            height: 150,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Search Tab',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color: Colors.green[800],
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Find the best content by searching here.',
+            style: TextStyle(
+              fontSize: 16,
+              fontStyle: FontStyle.italic,
+              color: Colors.green[600],
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(
+            'https://images.unsplash.com/photo-1511367461989-f85a21fda167?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D',
+            width: 150,
+            height: 150,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Profile Tab',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color: Colors.orange[800],
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Manage your profile and settings here.',
+            style: TextStyle(
+              fontSize: 16,
+              fontStyle: FontStyle.italic,
+              color: Colors.orange[600],
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
