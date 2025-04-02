82eb6d7dd55f869c0bd553d40eb8e74619ea611a
turzo1061 <turzo15-5785@diu.edu.bd>
2025-04-02 13:42:46 +0600

    Problem7

--- tracker_sheet_problems-ec3a87df2a65eda53dead0899a1bc8a41b5bae18/lib/main.dart
+++ tracker_sheet_problems-ec3a87df2a65eda53dead0899a1bc8a41b5bae18/lib/main.dart
@@ -4,163 +4,112 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
-  bool _isDarkMode = false;
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: Duration(milliseconds: 500),
-      vsync: this,
-    );
-  }
-
-  void _toggleDarkMode(bool value) {
-    setState(() {
-      _isDarkMode = value;
-      _isDarkMode ? _controller.forward() : _controller.reverse();
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _controller,
-      builder: (context, child) {
-        return MaterialApp(
-          title: 'Settings Screen',
-          theme: ThemeData(
-            brightness: _isDarkMode ? Brightness.dark : Brightness.light,
-            primarySwatch: Colors.blue,
-            useMaterial3: true,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ImageGridScreen(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.grey[50],
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.tealAccent[700], // Vibrant color for AppBar
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
           ),
-          home: SettingsScreen(
-            isDarkMode: _isDarkMode,
-            onToggleDarkMode: _toggleDarkMode,
+        ),
+        cardTheme: CardTheme(
+          shadowColor: Colors.black.withAlpha((0.1 * 255).toInt()),
+          elevation: 8,
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
 
-class SettingsScreen extends StatefulWidget {
-  final bool isDarkMode;
-  final Function(bool) onToggleDarkMode;
-
-  SettingsScreen({required this.isDarkMode, required this.onToggleDarkMode});
-
+class ImageGridScreen extends StatefulWidget {
   @override
-  _SettingsScreenState createState() => _SettingsScreenState();
+  _ImageGridScreenState createState() => _ImageGridScreenState();
 }
 
-class _SettingsScreenState extends State<SettingsScreen> with SingleTickerProviderStateMixin {
-  late AnimationController _drawerController;
-
-  @override
-  void initState() {
-    super.initState();
-    _drawerController = AnimationController(
-      duration: Duration(milliseconds: 400),
-      vsync: this,
-    );
-  }
-
-  void _toggleDrawer() {
-    if (_drawerController.status == AnimationStatus.completed) {
-      _drawerController.reverse();
-    } else {
-      _drawerController.forward();
-    }
-  }
+class _ImageGridScreenState extends State<ImageGridScreen> {
+  final List<String> imageUrls = [
+    "https://images.unsplash.com/photo-1580477667995-2b94f01c9516?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1615592389070-bcc97e05ad01?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1578632767115-351597cf2477?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1575540325855-4b5d285a3845?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Settings'),
-        backgroundColor: Colors.blueAccent,
-        leading: IconButton(
-          icon: Icon(Icons.menu, color: Colors.white),
-          onPressed: _toggleDrawer,
-        ),
+        title: Text("AnimeCorner"),
       ),
-      body: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Appearance',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.blueAccent),
-                ),
-                SizedBox(height: 10),
-                Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-                  child: ListTile(
-                    leading: Icon(Icons.dark_mode, color: Colors.blueAccent),
-                    title: Text('Dark Mode', style: TextStyle(fontSize: 18)),
-                    trailing: Switch(
-                      value: widget.isDarkMode,
-                      onChanged: widget.onToggleDarkMode,
-                      activeColor: Colors.blueAccent,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 15,
+            mainAxisSpacing: 15,
+            childAspectRatio: 1.0,
           ),
-          AnimatedBuilder(
-            animation: _drawerController,
-            builder: (context, child) {
-              return Transform.translate(
-                offset: Offset(-MediaQuery.of(context).size.width * (1 - _drawerController.value), 0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.75,
-                  child: Drawer(
-                    child: ListView(
-                      padding: EdgeInsets.zero,
-                      children: [
-                        DrawerHeader(
-                          decoration: BoxDecoration(color: Colors.blueAccent),
-                          child: Text(
-                            'Menu',
-                            style: TextStyle(color: Colors.white, fontSize: 24),
+          itemCount: imageUrls.length,
+          itemBuilder: (context, index) {
+            String imagePath = imageUrls[index];
+
+            return MouseRegion(
+              onEnter: (_) {
+                setState(() {});
+              },
+              onExit: (_) {
+                setState(() {});
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+                transform: Matrix4.identity()..scale(1.05),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  elevation: 12,
+                  shadowColor: Colors.black.withAlpha((0.1 * 255).toInt()),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.network(
+                      imagePath,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                      loadingBuilder: (context, child, loadingProgress) {
+                        if (loadingProgress == null) return child;
+                        return Center(
+                          child: CircularProgressIndicator(
+                            value: loadingProgress.expectedTotalBytes != null
+                                ? loadingProgress.cumulativeBytesLoaded /
+                                    (loadingProgress.expectedTotalBytes ?? 1)
+                                : null,
                           ),
-                        ),
-                        ListTile(
-                          leading: Icon(Icons.home),
-                          title: Text('Home'),
-                          onTap: () {},
-                        ),
-                        ListTile(
-                          leading: Icon(Icons.settings),
-                          title: Text('Settings'),
-                          onTap: () {},
-                        ),
-                        ListTile(
-                          leading: Icon(Icons.logout),
-                          title: Text('Logout'),
-                          onTap: () {},
-                        ),
-                      ],
+                        );
+                      },
                     ),
                   ),
                 ),
-              );
-            },
-          ),
-        ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
