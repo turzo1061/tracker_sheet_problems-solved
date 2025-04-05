127fb9a8844f82b6358001267aea3e4593509398
turzo1061 <turzo15-5785@diu.edu.bd>
2025-04-05 23:25:08 +0600

    Problem11

--- tracker_sheet_problems-ec3a87df2a65eda53dead0899a1bc8a41b5bae18/lib/main.dart
+++ tracker_sheet_problems-ec3a87df2a65eda53dead0899a1bc8a41b5bae18/lib/main.dart
@@ -1,34 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ImageGridScreen(),
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        scaffoldBackgroundColor: Colors.grey[50],
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.tealAccent[700], // Vibrant color for AppBar
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 24,
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('First Application'),
+          centerTitle: true, 
+          backgroundColor: const Color.fromARGB(255, 59, 147, 255),
         ),
-        cardTheme: CardTheme(
-          shadowColor: Colors.black.withAlpha((0.1 * 255).toInt()),
-          elevation: 8,
-          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
+        body: const Center(
+          child: Text(
+            'Hello World!',
+            style: TextStyle(
+              fontSize: 30,
+              fontFamily: 'DMSans',
+              color: Color.fromARGB(255, 226, 182, 47),
+            ),
           ),
         ),
       ),
@@ -36,81 +32,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ImageGridScreen extends StatefulWidget {
-  @override
-  _ImageGridScreenState createState() => _ImageGridScreenState();
-}
-
-class _ImageGridScreenState extends State<ImageGridScreen> {
-  final List<String> imageUrls = [
-    "https://images.unsplash.com/photo-1580477667995-2b94f01c9516?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1615592389070-bcc97e05ad01?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1578632767115-351597cf2477?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1575540325855-4b5d285a3845?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("AnimeCorner"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 15,
-            mainAxisSpacing: 15,
-            childAspectRatio: 1.0,
-          ),
-          itemCount: imageUrls.length,
-          itemBuilder: (context, index) {
-            String imagePath = imageUrls[index];
-
-            return MouseRegion(
-              onEnter: (_) {
-                setState(() {});
-              },
-              onExit: (_) {
-                setState(() {});
-              },
-              child: AnimatedContainer(
-                duration: Duration(milliseconds: 300),
-                curve: Curves.easeInOut,
-                transform: Matrix4.identity()..scale(1.05),
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  elevation: 12,
-                  shadowColor: Colors.black.withAlpha((0.1 * 255).toInt()),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: Image.network(
-                      imagePath,
-                      fit: BoxFit.cover,
-                      width: double.infinity,
-                      height: double.infinity,
-                      loadingBuilder: (context, child, loadingProgress) {
-                        if (loadingProgress == null) return child;
-                        return Center(
-                          child: CircularProgressIndicator(
-                            value: loadingProgress.expectedTotalBytes != null
-                                ? loadingProgress.cumulativeBytesLoaded /
-                                    (loadingProgress.expectedTotalBytes ?? 1)
-                                : null,
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
